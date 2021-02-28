5cc782a65161659dba9b2082c47ef0c1e661d972
Dmitry Slobodchikov <zoosman@mail.ru>
2021-02-28 19:22:16 +0500

    Comply task 07-03_spruce_tree

--- lesson_07/003/main.go
+++ lesson_07/003/main.go
@@ -16,7 +16,22 @@ func main() {
 	fmt.Println("---------------------")
 
 	for i := 0; i < height; i++ {
-
+		var branch []rune
+		// left branch
+		for k := 0; k < height+1-i; k++ {
+			branch = append(branch, ' ')
+		}
+		for k := 0; k < i+1; k++ {
+			branch = append(branch, '*')
+		}
+		// right branch
+		for k := 0; k < i; k++ {
+			branch = append(branch, '*')
+		}
+		for k := 0; k < height-i; k++ {
+			branch = append(branch, ' ')
+		}
+		fmt.Println(string(branch))
 	}
 
 	fmt.Println("---------------------")
