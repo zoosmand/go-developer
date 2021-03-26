10c7aeb7f24679cf9e8fcf322037aa50f3c17c06
Dmitry Slobodchikov <zoosman@mail.ru>
2021-03-26 16:24:02 +0500

    Comply task 07-05_find_number

--- lesson_07/005/main.go
+++ lesson_07/005/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
 	// Introduction
 	fmt.Println()
 	fmt.Print("Задача 7.5. Угадай число\n\n")
+
+	rand.Seed(time.Now().UnixNano())
+	secretNumber := rand.Intn(101)
 
 	var number int
 	fmt.Println("Введите номер от 1 до 100: ")
@@ -16,9 +21,18 @@ func main() {
 		_, _ = fmt.Scan(&number)
 
 		if number >= 1 && number <= 100 {
-			break
+			if secretNumber > number {
+				fmt.Println("Больше!")
+			} else if secretNumber < number {
+				fmt.Println("Меньше!")
+			} else if secretNumber == number {
+				fmt.Println("Найдено!")
+				break
+			}
+
+		} else {
+			fmt.Println("Неверное значение! Введите номер от 1 до 100: ")
 		}
-		fmt.Println("Неверное значение! Введите номер от 1 до 100: ")
 	}
 
 }
