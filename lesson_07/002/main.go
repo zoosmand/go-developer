efed3b2e41dae18088b8372db144fe959713bd96
Dmitry Slobodchikov <zoosman@mail.ru>
2021-03-26 13:39:10 +0500

    Fix task 07-02 add bitwise instead of func

--- lesson_07/002/main.go
+++ lesson_07/002/main.go
@@ -14,27 +14,16 @@ func main() {
 	fmt.Scan(&size)
 
 	fmt.Println("---------------------")
-
-	field := '*'
-	var toggleField = func() {
- 		if field == ' ' {
-			field = '*'
-		} else {
-			field = ' '
-		}
-	}
-
+	
 	for i := 0; i < size; i++ {
-		var line []rune
 		for k := 0; k < size; k++ {
-			toggleField()
-			line = append(line, field)
+			if (i^k)&1 == 0 {
+				fmt.Print(" ")
+			} else {
+				fmt.Print("*")
+			}
 		}
-		// Depending on parity skip the next field
-		if size%2 == 0 {
-			toggleField()
-		}
-		fmt.Println(string(line))
+		fmt.Print("\n")
 	}
 
 	fmt.Println("---------------------")
