f6c90ce2f84c909383d551549ca3551ea337119a
Dmitry Slobodchikov <zoosman@mail.ru>
2021-05-18 15:01:58 +0500

    Fix overfload

--- lesson_09/001/main.go
+++ lesson_09/001/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"math"
+	"math/bits"
 )
 
 func main() {
@@ -10,24 +10,9 @@ func main() {
 	fmt.Print("\nЗадача 9.1. Переполнение.\n")
 	fmt.Print("-------------------------\n\n")
 
-	var counterUint8 uint8
-	var counterUint16 uint16
+	counter8 := uint32(0xffffffff) >> uint32(bits.Len32(uint32(0xff)))
+	counter16 := uint32(0xffffffff) >> uint32(bits.Len32(uint32(0xffff)))
 
-	var counter8 uint32
-	var counter16 uint32
-
-	for i := 0; i < math.MaxUint32; i++ {
-		counterUint8++
-		counterUint16++
-
-		if counterUint8 == 0 {
-			counter8++
-		}
-		if counterUint16 == 0 {
-			counter16++
-		}
-	}
-
-	fmt.Println("Количество переполнений 8-ми битного беззнакового значения в 32-х битном беззнаковом поле: ", counter8+1)
-	fmt.Println("Количество переполнений 16-ти битного беззнакового значения в 32-х битном беззнаковом поле: ", counter16+1)
+	fmt.Println("Количество переполнений 8-ми битного беззнакового значения в 32-х битном беззнаковом поле: ", counter8)
+	fmt.Println("Количество переполнений 16-ти битного беззнакового значения в 32-х битном беззнаковом поле: ", counter16)
 }
