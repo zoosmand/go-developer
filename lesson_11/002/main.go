083c10c814b7874333b69d45bdc21e057a99a3e7
Dmitry Slobodchikov <zoosman@mail.ru>
2021-08-12 12:35:44 +0500

    Comply task 11-2_string_to_integer

--- lesson_11/002/main.go
+++ lesson_11/002/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
+	"strings"
 
 	usrcons "../../packages/usrcons"
 )
@@ -11,6 +14,31 @@ func main() {
 	h := "Задача 11.2. Строки."
 	usrcons.PrintHeader(&h)
 
+	myLine := "a10 10 20b 20 30c30 30 dd"
+	// myLine := "     "
+	// myLine := ""
+	myArray := strings.Split(myLine, " ")
+	myArraySize := len(myArray)
+	rule := regexp.MustCompile(`[a-f]`)
+
 	fmt.Println("------------------------------------------------")
+	for i := 0; i < myArraySize; i++ {
+		base := 10
+		if rule.MatchString(myArray[i]) {
+			base = 16
+		}
+
+		if myArray[i] == "" {
+			fmt.Println("Невозможно преобразовать пустое значение")
+		} else {
+			a, err := strconv.ParseInt(myArray[i], base, 0)
+			if err != nil {
+				fmt.Println("Не удалось преобразовать строку к числовому значению")
+			} else {
+				fmt.Printf("Число: %d\n", a)
+			}
+		}
+	}
+
 	fmt.Println("------------------------------------------------")
 }
